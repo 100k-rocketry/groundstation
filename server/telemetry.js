db65e8724e6d66746e7c506155e8f6f3eafed057
Albert Morgan <morgaalb@oregonstate.edu>
2017-02-06 15:19:19 -0800

    Added json-csv

--- server/telemetry.js
+++ server/telemetry.js
@@ -39,3 +39,10 @@ setInterval(() => { telemetryEmitter.sendPacket(); }, 1000);
 
 module.exports = telemetryEmitter;
 
+/* SERIAL PORT STUFF: WORK IN PROGRESS */
+
+var port = new SerialPort('COM1');
+
+port.on('open', function() {
+	console.log('Connected to serial port');
+});
