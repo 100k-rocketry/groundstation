6358ffcbc376a32480c3533328c74d338d23ed92
DESKTOP-IOSHS7E\tlee1 <terranceleeii13@gmail.com>
2017-02-06 14:26:27 -0800

    added the csv pipeline

--- server/logger.js
+++ server/logger.js
@@ -21,4 +21,18 @@ module.exports.logPacket = function(packet) {
 	if (fd != -1) {
 		fs.write(fd, packet.sensor + ', ' + packet.value + ', ' + packet.timestamp + os.EOL);
 	}
+	
+	var jsoncsv = require('json-csv')
+
+jsoncsv.csvBuffered(data, options, callback)
+
+var callback = function(err,csv) {
+ //csv contains string that are already converted to CSV format
+}
+
+var jsoncsv = require('json-csv)
+
+var readable_source = <emits data row by row>
+
+readable_source.pipe(jsoncsv.csv(info))
 }
