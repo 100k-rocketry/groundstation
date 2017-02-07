bebb1b30c9ea522f0ec7626cd7847658143b0873
DESKTOP-IOSHS7E\tlee1 <terranceleeii13@gmail.com>
2017-02-06 16:32:19 -0800

    adjusted the way the logger presents

--- server/logger.js
+++ server/logger.js
@@ -19,11 +19,12 @@ fs.open(logfile, 'a+', function(err, newfd) {
 console.log('Log file ' + logfile + 'opened.');
 
 module.exports.logPacket = function(packet) {
-	//if (fd != -1) {
+	if (fd != -1) {
 	//	fs.write(fd, packet.sensor + ', ' + packet.value + ', ' + packet.timestamp + os.EOL);
 	//}
-	
-	jsoncsv.csvBuffered([packet], {
+		fs.write(fd, packet.altimeter + ', ' + packet.latitude + ', ' + packet.longitude + ', ' + packet.accelerometer_x + ', ' + packet.accelerometer_y + ', ' 
+		+ packet.accelerometer_z ', ' + packet.yaw + ', ' + packet.pitch + ', ' + packet.roll + ', ' + packet.timestamp + ', ' + os.EOL  )}
+	/*jsoncsv.csvBuffered([packet], {
   fields : [
     {
         name : 'altimeter',
@@ -70,5 +71,5 @@ module.exports.logPacket = function(packet) {
   ]},
   function(err,csv) {
     console.log(csv);
-});
+});*/
 }
