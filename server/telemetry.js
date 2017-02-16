57950984308fec325f5bc2a655f4696cb1ec9a5c
Albert Morgan <morgaalb@oregonstate.edu>
2017-02-16 15:29:13 -0800

    Cleaned up telemetry and added mocked ata

--- server/telemetry.js
+++ server/telemetry.js
@@ -3,21 +3,22 @@ var SerialPort = require("usb");
  
 var telemetryEmitter = new EventEmitter();
 
-// This gets triggered when a new packet comes in.
-/*
-sensorEmitter.on("newPacket", function () {
-    console.log("event has occured");
-});
-*/
-
 telemetryEmitter.altitude = 0;
 telemetryEmitter.timestamp = 0;
+telemetryEmitter.latitude = 45.0;
+telemetryEmitter.longitude = 100;
+telemetryEmitter.accelerometer_x = 0;
+telemetryEmitter.accelerometer_y = 0;
+telemetryEmitter.accelerometer_z = 0;
+telemetryEmitter.yaw = 0;
+telemetryEmitter.pitch = 90;
+telemetryEmitter.roll = 0;
 
 // Sends a fake packet
 telemetryEmitter.sendPacket = function() {
 	this.emit("newPacket", 
 		{
-			"altimeter": 11,
+			"altimeter": this.altitude,
 			"latitude": 45.11,
 			"longitude": 100.12,
 			"accelerometer_x": 1,
@@ -26,22 +27,21 @@ telemetryEmitter.sendPacket = function() {
 			"yaw": 4,
 			"pitch": 5,
 			"roll": 6,
-			"timestamp": 7
+			"timestamp": this.timestamp
 		});
-	this.altitude += 10;
-	this.timestamp += 1;
+telemetryEmitter.altitude += 10;
+telemetryEmitter.timestamp += 1;
+telemetryEmitter.latitude += 0.01;
+telemetryEmitter.longitude -= 0.01;
+telemetryEmitter.accelerometer_x += 0.03;
+telemetryEmitter.accelerometer_y -= 0.02;
+telemetryEmitter.accelerometer_z += 1.134;
+telemetryEmitter.yaw += 0.0134;
+telemetryEmitter.pitch += 0.01934;
+telemetryEmitter.roll += 0.014343;
 };
 
 // Fire a fake packet every second
 setInterval(() => { telemetryEmitter.sendPacket(); }, 1000);
 
 module.exports = telemetryEmitter;
-
-/* SERIAL PORT STUFF: WORK IN PROGRESS */
-
-/*var port = new SerialPort('COM1');
-
-port.on('open', function() {
-	console.log('Connected to serial port');
-});*/
-
