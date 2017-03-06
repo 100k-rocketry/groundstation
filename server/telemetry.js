704cbce4db94201134b2450df5fb8db433f138c7
Albert Morgan <morgaalb@oregonstate.edu>
2017-03-06 14:39:24 -0800

    USB test

--- server/telemetry.js
+++ server/telemetry.js
@@ -1,5 +1,6 @@
 var EventEmitter = require("events").EventEmitter;
-var usb = require("usb");
+var dataEmitter = require("./data");
+var usb = require('usb');
 
 var telemetryEmitter = new EventEmitter();
 
@@ -42,12 +43,61 @@ telemetryEmitter.sendPacket = function() {
 };
 
 // Fire a fake packet every second
-setInterval(() => { telemetryEmitter.sendPacket(); }, 1000);
+//setInterval(() => { telemetryEmitter.sendPacket(); }, 1000);
+
+dataEmitter.on('data', function(data) {
+	var mode = -1;
+	var altitude = -1;
+	var latitude = -1;
+	var longitude = -1;
+	var accelerometer_x = -1;
+	var accelerometer_y = -1;
+	var accelerometer_z = -1;
+	var yaw = -1;
+	var pitch = -1;
+	var roll = -1;	
+	
+	// mode
+	mode = data.readUInt8(0);
+	// altimeter
+	altitude = data.readUInt16LE(1);
+	altitude += data.readUInt8(3) * 65536;
+	// lat
+	latitude = data.readFloatLE(4);
+	// long
+	longitude = data.readFloatLE(8);
+	// accel x
+	accelerometer_x = data.readInt16LE(12);
+	// accel y
+	accelerometer_y = data.readInt16LE(14);
+	// accel z
+	accelerometer_z = data.readInt16LE(16);
+	// gyro x
+	yaw = data.readInt16LE(18);
+	// gyro y
+	pitch = data.readInt16LE(20);
+	// gyro z
+	roll = data.readInt16LE(22);
+	
+	telemetryEmitter.emit('newPacket', {
+		"altimeter": altitude,
+		"latitude": latitude,
+		"longitude": longitude,
+		"accelerometer_x": accelerometer_x,
+		"accelerometer_y": accelerometer_y,
+		"accelerometer_z": accelerometer_z,
+		"yaw": yaw,
+		"pitch": pitch,
+		"roll": roll,
+		"timestamp": 0
+	});
+	
+	console.log(mode, altitude, latitude, longitude, accelerometer_x, accelerometer_y, accelerometer_z, yaw, pitch, roll);
+});
 
 module.exports = telemetryEmitter;
 
 usb.on('attach', function(device) {
-
 	device.open();
 	var iface = device.interface(1);
 	iface.detachKernelDriver();
@@ -58,5 +108,4 @@ usb.on('attach', function(device) {
 	endpoint.on('data', function(d) {
 		console.log(d);
 	});
-
 });
