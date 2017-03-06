39777719b194cc8aed3a0852722d89c23dc6f762
Terrance Lee <terranceleeii13@gmail.com>
2017-03-06 14:19:23 -0800

    update for reboot

--- server/Reboot.js
+++ server/Reboot.js
@@ -1,4 +1,44 @@
-initctl status node
-reboot
-initctl status node
-nano /etc/init/node.conf
+#Level to run in
+id:3:initdefault:
+
+#what to do in single user mode?
+~:S:wait:/sbin/sulogin
+
+# /etc/init.d executes the S and K scripts upon change
+# of runlevel.
+#
+# Default runlevel. The runlevels used by RHS are:
+#   0 - halt (Do NOT set initdefault to this)
+#   1 - Single user mode
+#   2 - Multiuser, without NFS (The same as 3, if you do not have networking)
+#   3 - Full multiuser mode
+#   4 - unused
+#   5 - X11
+#   6 - reboot (Do NOT set initdefault to this)
+	
+# Boot-time system configuration/initialization script.
+si::sysinit:/etc/rc.d/rc.sysinit
+
+l0:0:wait:/etc/rc 0
+l1:1:wait:/etc/rc 1
+l2:2:wait:/etc/rc 2
+l3:3:wait:/etc/rc 3
+l4:4:wait:/etc/rc 4
+l5:5:wait:/etc/rc 5
+l6:6:wait:/etc/rc 6
+
+# Things to run in every runlevel.
+ud::once:/sbin/update
+
+# Trap CTRL-ALT-DELETE
+ca::ctrlaltdel:/sbin/shutdown -t3 -r now
+
+
+1:2345:respawn:/sbin/mingetty tty1
+2:2345:respawn:/sbin/mingetty tty2
+3:2345:respawn:/sbin/mingetty tty3
+4:2345:respawn:/sbin/mingetty tty4
+5:2345:respawn:/sbin/mingetty tty5
+6:2345:respawn:/sbin/mingetty tty6
+
+npm start
