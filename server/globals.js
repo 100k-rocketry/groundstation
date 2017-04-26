b745d306df55d666635010979c3ff2a361b93cb2
Albert Morgan <morgaalb@oregonstate.edu>
2017-04-26 15:09:40 -0700

    Annotated globals

--- server/globals.js
+++ server/globals.js
@@ -2,7 +2,8 @@ var now = new Date();
 
 module.exports = {
 	// Set this to false to use the mocked data instead of trying to connect to the actual telemtry device
-	useUSB: true,
+	useUSB: false,
+	// The time that the server started up (used for the log filename)
 	time: now.toISOString(),
 	logFilename: ('log-' + now.toISOString() + '.csv').replace(/:/g, '.'),
 	// The MAC addresses of the sustainer and booster
