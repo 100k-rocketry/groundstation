4b92de2715682eacd0dfd2e7e26812b4ec8687cd
Albert Morgan <morgaalb@oregonstate.edu>
2017-04-27 15:41:31 -0700

    Added /logs path

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var telemetryEmitter = require('./telemetry');
 var mocker = require('./mocker');
 var globals = require('./globals');
+var fs = require('fs');
 require('./logger');
 
 // Get environment variables
@@ -17,6 +18,8 @@ var expressWs = require('express-ws')(app);
 
 var allPackets = [];
 
+var header = fs.readFileSync(path.join(__dirname, 'templates', 'header.html'), 'utf8');
+var footer = fs.readFileSync(path.join(__dirname, 'templates', 'footer.html'), 'utf8');
 
 telemetryEmitter.on("newPacket", (packet) => {
 	//console.log(packet);
@@ -66,8 +69,52 @@ app.ws('/', function(ws, req) {
 	});
 });
 
+// If the client navigates to 'pages/page_nm', open the appropriate page
+// from the templates folder and send it. If it's not found, then next()
+// to the 404.
+app.get('/pages/:page', function (req, res, next) {
+	var page = req.params.page + '.html';
+
+	if (page) {
+		var content = header;
+		fs.readFile(path.join(__dirname, 'templates', page), (err, data) => {
+			if(!err) {
+				res.write(header);
+				res.write(data);
+				res.write(footer);
+				res.end();
+			} else {
+				next();
+			}
+		});
+	} else {
+		next();
+	}
+});
+
+
+app.get('/logs', function (req, res, next) {
+	fs.readdir('logs', function(err, files) {
+		files.forEach(function(f) {
+			res.write('<a href="logs/' + f + '">' + f + '</a><br>');
+		});
+		res.end();
+	});
+});
+
+
+app.get('/logs/:log', function (req, res, next) {
+	var log = req.params.log;
+
+	if (req.params.log) {
+		res.download (path.join(__dirname, 'logs', log));
+	} else {
+		next();
+	}
+});
+
 app.get('/log', function(req, res) {
-	res.sendFile('/home/pi/groundstation/server/' + globals.logFilename);
+	res.sendFile(__dirname + globals.logFilename);
 });
 
 // Set up the static routes for the web server
